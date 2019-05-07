e9f64d1de80f3b6a459e30da63560f9b36d96ee2
jeremy-woods <jdwoods2008@gmail.com>
2019-05-06 19:39:01 -0500

    added firebase config

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDEimf9D2q6f2lYuhexOctizAn6HWdcMnI",
+    authDomain: "thats-handy.firebaseapp.com",
+    databaseURL: "https://thats-handy.firebaseio.com",
+    projectId: "thats-handy",
+    storageBucket: "thats-handy.appspot.com",
+    messagingSenderId: "126210326319"
+}
